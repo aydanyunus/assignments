353a70d1333de5c4a544acfed449090d1d35ff91
aydanyunus <aydanyunus98@gmail.com>
2023-10-08 01:54:53 +0400

    modified

--- hw_node_05/src/routes/news.route.ts
+++ hw_node_05/src/routes/news.route.ts
@@ -1,27 +1,60 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import newsService from "../services/news.service.ts";
 
 const newsRouter = Router();
 
-newsRouter.get("/", async (req, res) => {
-  const news = await newsService.getNews();
-  res.setHeader("content-type", "application/json").json(news);
+newsRouter.get("/", async (req: Request, res: Response) => {
+  try {
+    const { page, size } = req.query;
+    const news = await newsService.getNews({ page, size });
+    res.setHeader("content-type", "application/json").status(200).json(news);
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
 });
 
-newsRouter.get('/:id', async (req,res)=> {
-  const news = await newsService.getNewsByID(+req.params.id);
-  res.setHeader("content-type", "application/json").json(news);
-})
+newsRouter.get("/:id", async (req: Request, res: Response) => {
+  try {
+    const news = await newsService.getNewsByID(+req.params.id);
+    if (!news) {
+      res.status(404).json({ error: "News not found" });
+    } else {
+      res.status(200).json(news);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
+// post not working
+newsRouter.post("/", async (req: Request, res: Response) => {
+  try {
+    const newPost = await newsService.addNews(req.body);
+    res.status(201).json({ message: "created!", newPost });
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
-newsRouter.post('/',async (req, res) => {
-  const newPost = await newsService.addNews(req.body);
-  res.status(200).json({ msg: "created!", newPost }); 
-} )
+newsRouter.put("/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await newsService.editNews(+req.params.id, req.body);
+    res.setHeader("content-type", "application/json").status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
-
-// newsRouter.get('/:id', )
-// newsRouter.put('/:id', )
-// newsRouter.delete('/:id', );
+newsRouter.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await newsService.deleteNews(+req.params.id);
+    return res
+      .setHeader("content-type", "application/json")
+      .status(200)
+      .json(result);
+  } catch (error) {
+    res.status(500).json({ error: "server error" });
+  }
+});
 
 export default newsRouter;
--- hw_node_05/src/services/news.service.ts
+++ hw_node_05/src/services/news.service.ts
@@ -1,14 +1,15 @@
 import { AppDataSource } from "../data-source.ts";
 import { News } from "../entity/news.entity.ts";
 
-interface IPost {
-    title: string,
-    text: string
-}
 
-const getNews = async () => {
+
+const getNews = async ({ page, size }) => {
   const news = await AppDataSource.getRepository(News).find();
-  return news;
+
+  const startIndex = (page - 1) * size;
+  const endIndex = page * size;
+  const data = news.slice(startIndex, endIndex);
+  return data;
 };
 
 const getNewsByID = async (id: number) => {
@@ -16,14 +17,30 @@ const getNewsByID = async (id: number) => {
   return news;
 };
 
-const addNews = async (body: IPost) => {
-    const newPost = await AppDataSource.getRepository(News).create(body);
-    const result = await AppDataSource.getRepository(News).save(newPost);
-    return result;
-  };
+const addNews = async (body) => {
+  const newPost = await AppDataSource.getRepository(News).create(body);
+    const results = await AppDataSource.getRepository(News).save(newPost)
+    return results
+}
+
+const editNews = async (id: number, body) => {
+  const editedNews = await AppDataSource.getRepository(News).findOneBy({
+    id,
+  });
+  AppDataSource.getRepository(News).merge(editedNews, body);
+  const result = await AppDataSource.getRepository(News).save(editedNews);
+  return result;
+};
+
+const deleteNews = async (id: number) => {
+  const results = await AppDataSource.getRepository(News).delete(id);
+  return results;
+};
 
 export default {
   getNews,
   getNewsByID,
-  addNews
+  addNews,
+  editNews,
+  deleteNews,
 };
